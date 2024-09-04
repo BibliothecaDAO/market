a807d4a3ef0e52c51b557ca05fba36fb946c0aad
Valentin Dosimont <valentin@pupucecorp.com>
2024-09-05 00:06:15 +0200

    fix: controller rpc fix

--- apps/arkmarket/src/components/connectors.tsx
+++ apps/arkmarket/src/components/connectors.tsx
@@ -1,11 +1,12 @@
 import CartridgeConnector from "@cartridge/connector";
-import { Connector } from "@starknet-react/core";
+import type { Connector } from "@starknet-react/core";
+import { env } from "~/env";
 
 export const getConnectors = (): { connectors: Connector[] } => {
-  const paymaster: any = { caller: "0x414e595f43414c4c4552" };
+  // const paymaster: any = { caller: "0x414e595f43414c4c4552" };
   //   const options: any = { theme: "paved", paymaster };
   const cartridge = new CartridgeConnector({
-    rpc: process.env.NEXT_PUBLIC_RPC_URL,
+    rpc: env.NEXT_PUBLIC_RPC_URL + "?apikey=" + env.NEXT_PUBLIC_RPC_API_KEY,
   }) as never as Connector;
   return { connectors: [cartridge] };
 };
