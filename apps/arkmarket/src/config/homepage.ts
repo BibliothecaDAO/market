1811e83996379ff3c85d1405a6cbda97c9c22302
Valentin Dosimont <valentin@pupucecorp.com>
2024-12-04 00:35:45 +0100

    feat: add eternum season pass collection

--- apps/arkmarket/src/config/homepage.ts
+++ apps/arkmarket/src/config/homepage.ts
@@ -9,6 +9,7 @@ export enum Collections {
   DUNGEON_DUCKS = "dungeonducks",
   KARAT = "karat",
   JOKERSOFNEON = "jokersofneon",
+  ETERNUMSEASONPASS = "eternumseasonpass",
 }
 
 export enum ChainId {
@@ -87,6 +88,11 @@ export const CollectionAddresses: {
       "0x07268fcf96383f8691b91ba758cc8fefe0844146f0557909345b841fb1de042f",
     [ChainId.SN_SEPOLIA]: "",
   },
+  [Collections.ETERNUMSEASONPASS]: {
+    [ChainId.SN_MAIN]:
+      "0x057675b9c0bd62b096a2e15502a37b290fa766ead21c33eda42993e48a714b80",
+    [ChainId.SN_SEPOLIA]: "",
+  },
 };
 
 export const CollectionNames = {
@@ -100,6 +106,7 @@ export const CollectionNames = {
   [Collections.DUNGEON_DUCKS]: "Dungeon Ducks",
   [Collections.KARAT]: "Karat",
   [Collections.JOKERSOFNEON]: "Jokers of Neon x Loot Survivor: Beasts",
+  [Collections.ETERNUMSEASONPASS]: "Eternum Season Pass",
 };
 
 interface CollectionDescription {
@@ -168,6 +175,12 @@ export const CollectionDescription: Record<string, CollectionDescription> = {
     description:
       "Jokers of Neon mod exclusive collection",
   },
+  // @ts-expect-error It's ok compiler
+  [CollectionAddresses[Collections.ETERNUMSEASONPASS][ChainId.SN_MAIN]]: {
+    created: "2024",
+    description:
+      "Eternum Season Pass",
+  },
 };
 
 export const homepageConfig = {
@@ -206,6 +219,15 @@ export const homepageConfig = {
       description: "Syndicate",
       address: CollectionAddresses[Collections.SYNDICATE][ChainId.SN_MAIN],
       itemsCount: 1600,
+      // floorPrice: 0.12,
+    },
+    {
+      bannerSrc: "/banners/realms.png",
+      collectionSrc: "/collections/eternum-season-pass.png",
+      name: CollectionNames[Collections.ETERNUMSEASONPASS],
+      description: "Eternum Season Pass",
+      address: CollectionAddresses[Collections.ETERNUMSEASONPASS][ChainId.SN_MAIN],
+      itemsCount: 0,
       // floorPrice: 0.12,
     },
     {
@@ -265,6 +287,12 @@ export const homepageConfig = {
       name: CollectionNames[Collections.REALMS],
       address: CollectionAddresses[Collections.REALMS][ChainId.SN_MAIN],
       image: "/collections/realms.png",
+      banner_image: "/banners/realms.png",
+    },
+    {
+      name: CollectionNames[Collections.ETERNUMSEASONPASS],
+      address: CollectionAddresses[Collections.ETERNUMSEASONPASS][ChainId.SN_MAIN],
+      image: "/collections/eternum-season-pass.png",
       banner_image: "/banners/realms.png",
     },
     {
